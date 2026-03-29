74f952b822d104053bd785a167fa4e1ee9f2f816
Nico <nicox.lobosg@gmail.com>
2026-03-29 17:04:00 -0300

    Diseño principal barra lateral

--- Frontend/src/Componentes/Inicio.jsx
+++ Frontend/src/Componentes/Inicio.jsx
@@ -1,7 +1,33 @@
+import { useState } from "react";
+import "../Css/BarraLateral.css";
+
 function Inicio() {
+    const [abrirBarra, setAbrirBarra] = useState(false);
+
     return (
-        <div>
-            <h1>Bienvenido a la página de inicio</h1>
+        <div className="inicio">
+            <header className="headerBarraLateral">
+                <div className="izquierda">
+                    <button className="botonMenu" onClick={() => setAbrirBarra(!abrirBarra)}> 
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+                </div>
+
+                <div className="derecha">
+                    <p className="usuario">Usuario</p>
+                </div>
+            </header>
+
+            <div className={`barra-lateral ${abrirBarra ? "activa" : ""}`}>
+                <h2>Canales</h2>
+                <ul>
+                    <li>Opción 1</li>
+                    <li>Opción 2</li>
+                    <li>Opción 3</li>
+                </ul>
+            </div>
         </div>
     )
 }
